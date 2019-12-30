bb07ef38252c50c1948812703c9cf55ef454e8df
omsoo <43151556+omsoo@users.noreply.github.com>
2019-12-30 13:55:27 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function userValidationForm() {
-  var nameInput = document.forms["userForm"]["uname"].value;
-  if (nameInput == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
